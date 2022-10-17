9ce78c83df0bd94ec17ee4011e91bc647f7e50a9
RichardKovacik <56000073+RichardKovacik@users.noreply.github.com>
2022-10-17 18:53:09 +0200

    Initial commit

---
+++ src/test/java/sk/fri/uniza/coffeSiTy/CoffeSiTyApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.fri.uniza.coffeSiTy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeSiTyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
