ee7d0440230056e70d539bc371d3f377b39e3c02
RichardKovacik <56000073+RichardKovacik@users.noreply.github.com>
2022-10-23 14:03:13 +0200

    Pridane stranka notFound.html +refator

---
+++ src/main/java/sk/fri/uniza/coffeSiTy/controllers/PageNotFoundController.java
@@ -0,0 +1,29 @@
+package sk.fri.uniza.coffeSiTy.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PageNotFoundController implements org.springframework.boot.web.servlet.error.ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                // return "error-404";
+                return "notFound";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500";
+            }
+        }
+        return "error";
+    }
+}
