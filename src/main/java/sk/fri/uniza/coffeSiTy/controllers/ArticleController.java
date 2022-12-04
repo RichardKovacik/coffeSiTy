c251c6f1ce065b9e5a6073f14025c2653249378a
RichardKovacik <56000073+RichardKovacik@users.noreply.github.com>
2022-12-04 11:47:16 +0100

    Logika zobrazenia btns pri clanoch.

--- src/main/java/sk/fri/uniza/coffeSiTy/controllers/ArticleController.java
+++ src/main/java/sk/fri/uniza/coffeSiTy/controllers/ArticleController.java
@@ -1,6 +1,8 @@
 package sk.fri.uniza.coffeSiTy.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -8,7 +10,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import sk.fri.uniza.coffeSiTy.entity.Article;
+import sk.fri.uniza.coffeSiTy.entity.User;
 import sk.fri.uniza.coffeSiTy.service.ArticleService;
+import sk.fri.uniza.coffeSiTy.service.UserService;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -18,11 +22,20 @@ public class ArticleController {
     @Autowired
     ArticleService articleService;
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping("/blog")
     @Transactional
     public String getAllArticles(Model model) {
         List<Article> listArticles = articleService.getAllArticles();
         model.addAttribute("list", listArticles);
+        //vytiahnem z db aktualne prihlseneho usera
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByNick(auth.getName());
+        if (user!= null){
+            model.addAttribute("user",user);
+        }
         return "blog";
     }
     @RequestMapping("/addArticle")
@@ -38,7 +51,12 @@ public class ArticleController {
                                BindingResult result,
                                Model model) {
         //todo: validacie
-        //ulozim article s danym autorom kotry pridal slanok
+        //ulozim article s danym autorom kotry pridal clanok(moze len prihlaseny uzivatel)
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByNick(auth.getName());
+        if (user != null) {
+            article.setUser(user);
+        }
         articleService.saveArticle(article);
         return "redirect:/addArticle?success";
     }
--- src/main/java/sk/fri/uniza/coffeSiTy/service/ArticleService.java
+++ src/main/java/sk/fri/uniza/coffeSiTy/service/ArticleService.java
@@ -1,13 +1,9 @@
 package sk.fri.uniza.coffeSiTy.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import sk.fri.uniza.coffeSiTy.entity.Article;
-import sk.fri.uniza.coffeSiTy.entity.User;
 import sk.fri.uniza.coffeSiTy.repository.ArticleRepo;
-import sk.fri.uniza.coffeSiTy.repository.UserRepo;
 
 import java.util.List;
 
@@ -15,21 +11,11 @@ import java.util.List;
 public class ArticleService {
     @Autowired
     ArticleRepo articleRepo;
-
-    @Autowired
-    UserRepo userRepo;
-
     public List<Article> getAllArticles() {
         return articleRepo.findAll();
     }
 
     public void saveArticle(Article article) {
-        //zistim usernam aktualne prihlaseneho uzivatela
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user = userRepo.findUserByNick(auth.getName());
-        if (user != null){
-            article.setUser(user);
-        }
         articleRepo.save(article);
 
     }
