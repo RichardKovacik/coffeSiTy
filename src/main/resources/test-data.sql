3848f85e5c69619f412e1e1b13b9676e33dc844f
RichardKovacik <56000073+RichardKovacik@users.noreply.github.com>
2022-12-24 11:52:36 +0100

    Testing persistance layer with jpa tests refactor

--- src/main/resources/test-data.sql
+++ src/main/resources/test-data.sql
@@ -14,6 +14,9 @@ INSERT INTO `city` VALUES (1,'Jablonec',1),
 /* nainsrtovanie adries*/
 INSERT INTO `address` VALUES (1,'95636','borcany 12',2),(2,'95311','rybany 12',2),(3,'12345','mojmirova 5',4);
 
-/* nainserovanie userov nick=filip email:masaryk@azet.com*/
+/* nainserovanie 2 userov do testovacej databazy nick=filip email:masaryk@azet.com*/
 INSERT INTO `user` VALUES (1,'1965-12-15','masaryk@azet.com','masaryk','samuel',
-                           'filip','$2a$10$PDoeQUdzb/Xgi7vMUIavhePBEZmyW7gaifnq3xNvR59hvp.Rsw1cG',3)
+                           'filip','$2a$10$PDoeQUdzb/Xgi7vMUIavhePBEZmyW7gaifnq3xNvR59hvp.Rsw1cG',3);
+
+INSERT INTO `user` VALUES (2,'1999-12-15','lukas@azet.com','findor','lukas',
+                           'lukas3','$2a$10$PDoeQUdzb/Xgi7vMUIavhePBEZmyW7gaifnq3xNvR59hvp.Rsw1cG',1);
--- src/test/java/sk/fri/uniza/coffeSiTy/dataLayerTests/UserRepositoryTest.java
+++ src/test/java/sk/fri/uniza/coffeSiTy/dataLayerTests/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package sk.fri.uniza.coffeSiTy.dataLayerTests;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -9,18 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.Repeat;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.jdbc.JdbcTestUtils;
-import sk.fri.uniza.coffeSiTy.entity.Address;
-import sk.fri.uniza.coffeSiTy.entity.Article;
 import sk.fri.uniza.coffeSiTy.entity.User;
-import sk.fri.uniza.coffeSiTy.repository.AddressRepo;
 import sk.fri.uniza.coffeSiTy.repository.UserRepo;
 
 import java.util.List;
-import java.util.Optional;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -29,7 +25,7 @@ public class UserRepositoryTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
     @Autowired
-    UserRepo userRepo;
+    private UserRepo userRepo;
 
     @AfterEach
     void zmazData() {
@@ -41,47 +37,43 @@ public class UserRepositoryTest {
     @Sql("classpath:test-data.sql")
     public void testujVratenieZoznamuUserov() {
         int sizeOfList = userRepo.findAll().size();
-        Assertions.assertTrue(sizeOfList > 0);
+        Assertions.assertEquals(2, sizeOfList);
     }
     @Test
     @Sql("classpath:test-data.sql")
-    public void testujVymazeniUseraZdb() {
+    public void testujVymazeniUseraSId1Zdb() {
         long idOfDeletedUser = userRepo.deleteUserById(1L);
-        Assertions.assertEquals(0, userRepo.findAll().size());
+        Assertions.assertEquals(1, idOfDeletedUser);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"filip", "lukas3"})
+    @Sql("classpath:test-data.sql")
+    public void testujVratenieUseraPodlaNicku(String nick) {
+        User user = userRepo.findUserByNick(nick);
+        Assertions.assertNotNull(user);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"masaryk@azet.com", "lukas@azet.com"})
+    @Sql("classpath:test-data.sql")
+    public void testujVratenieUseraPodlaEmailu(String email) {
+        User user = userRepo.findUserByEmail(email);
+        Assertions.assertNotNull(user);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"samu","sa", "samuel","s","sa","sam"})
+    @Sql("classpath:test-data.sql")
+    public void testujVratenieZoznamuUserovPodlaMenaObsahujucehoText(String input) {
+        List<User> ussersMatchnigKeys = userRepo.findUsersByKey(input);
+        Assertions.assertNotNull(ussersMatchnigKeys);
     }
 
     @Test
     @Sql("classpath:test-data.sql")
-    public void testujVratenieUseraPodlaNicku() {
-        User user = userRepo.findUserByNick("filip");
-        Assertions.assertNotNull(user);
-    }
-    @Test
-    @Sql("classpath:test-data.sql")
-    public void testujVratenieUseraPodlaEmailu() {
-        User user = userRepo.findUserByEmail("masaryk@azet.com");
-        Assertions.assertNotNull(user);
-    }
-
-    @Test
-    @Sql("classpath:test-data.sql")
-    public void testujUpdateMenaUsera() {
+    public void testujZmenuMenaUsera() {
         User user = userRepo.findUserByNick("filip");
         user.setName("adam");
         User updatedUser = userRepo.save(user);
         Assertions.assertEquals( "adam",updatedUser.getName());
     }
-    @Test
-    @Sql("classpath:test-data.sql")
-    public void testujVratenieUseraPodlaMenaObsahujucehoTextSa() {
-        List<User> ussersMatchnigKeys = userRepo.findUsersByKey("sa");
-        Assertions.assertNotNull(ussersMatchnigKeys);
-    }
-    @Test
-    @Sql("classpath:test-data.sql")
-    public void testujVratenieUseraPodlaMenaObsahujucehoTextSamue() {
-        List<User> ussersMatchnigKeys = userRepo.findUsersByKey("samue");
-        Assertions.assertNotNull(ussersMatchnigKeys);
-    }
-
 }
